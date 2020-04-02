a372b7e4cea0dab03d4d64d070331c47a95a0369
Norman Fomferra <norman.fomferra@gmail.com>
2020-04-02 16:22:33 +0200

    more test modes

--- test/core/tile_client.py
+++ test/core/tile_client.py
@@ -15,8 +15,12 @@ import numpy as np
 import requests
 import xarray as xr
 
+import zarr
 from xcube.core.dsio import rimraf
 from xcube.core.store import CubeStore
+
+DEFAULT_COMPRESSOR = zarr.Blosc()
+DEFAULT_COMPRESSOR_CONFIG = DEFAULT_COMPRESSOR.get_config()
 
 DEFAULT_TILE_SIZE = 1024
 
@@ -25,53 +29,94 @@ NUM_Y_TILES = 5
 NUM_TIMES = 10
 
 
-def test_async(port: int, tile_size: int, dataset_path: str, var_name: str):
+def get_tests():
+    return {
+        'async_r': test_async_r,
+        'async_rw': test_async_rw,
+        'async_rwc': test_async_rwc,
+        'store_r': test_store_r,
+        'store_rw': test_store_rw,
+        'store_rwc': test_store_rwc,
+    }
+
+
+def test_async_r(*args):
+    _test_async(*args, write=False, compress=False)
+
+
+def test_async_rw(*args):
+    _test_async(*args, write=True, compress=False)
+
+
+def test_async_rwc(*args):
+    _test_async(*args, write=True, compress=True)
+
+
+def test_store_r(*args):
+    _test_store(*args, write=False, compress=False)
+
+
+def test_store_rw(*args):
+    _test_store(*args, write=True, compress=False)
+
+
+def test_store_rwc(*args):
+    _test_store(*args, write=True, compress=True)
+
+
+def _test_async(port: int, tile_size: int, dataset_path: str, var_name: str, write=False, compress=False):
     """
     Code inspired by https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
     """
 
     var_path = os.path.join(dataset_path, var_name)
-    os.makedirs(var_path, exist_ok=True)
 
-    with open(os.path.join(dataset_path, '.zgroup'), 'w') as fp:
-        json.dump(
-            {
-                "zarr_format": 2
-            },
-            fp, indent=2)
-    with open(os.path.join(dataset_path, '.zattrs'), 'w') as fp:
-        json.dump({}, fp, indent=2)
-    with open(os.path.join(var_path, '.zarray'), 'w') as fp:
-        json.dump(
-            {
-                "chunks": [
-                    1,
-                    tile_size,
-                    tile_size
-                ],
-                "compressor": None,
-                "dtype": "<f8",
-                "fill_value": "NaN",
-                "filters": None,
-                "order": "C",
-                "shape": [
-                    NUM_TIMES,
-                    tile_size * NUM_Y_TILES,
-                    tile_size * NUM_X_TILES
-                ],
-                "zarr_format": 2
-            },
-            fp, indent=2)
-    with open(os.path.join(var_path, '.zattrs'), 'w') as fp:
-        json.dump(
-            {
-                "_ARRAY_DIMENSIONS": [
-                    "time",
-                    "y",
-                    "x"
-                ]
-            },
-            fp, indent=2)
+    if write:
+        # mkdirs cube/var
+        os.makedirs(var_path, exist_ok=True)
+        # write cube/.zgroup
+        with open(os.path.join(dataset_path, '.zgroup'), 'w') as fp:
+            json.dump(
+                {
+                    "zarr_format": 2
+                },
+                fp, indent=2)
+        # write cube/.zattrs
+        with open(os.path.join(dataset_path, '.zattrs'), 'w') as fp:
+            json.dump({}, fp, indent=2)
+        # write cube/var/.zarray
+        with open(os.path.join(var_path, '.zarray'), 'w') as fp:
+            json.dump(
+                {
+                    "chunks": [
+                        1,
+                        tile_size,
+                        tile_size
+                    ],
+                    "compressor": DEFAULT_COMPRESSOR_CONFIG if compress else None,
+                    "dtype": "<f8",
+                    "fill_value": "NaN",
+                    "filters": None,
+                    "order": "C",
+                    "shape": [
+                        NUM_TIMES,
+                        tile_size * NUM_Y_TILES,
+                        tile_size * NUM_X_TILES
+                    ],
+                    "zarr_format": 2
+                },
+                fp, indent=2)
+        # write cube/var/.zattrs
+        with open(os.path.join(var_path, '.zattrs'), 'w') as fp:
+            json.dump(
+                {
+                    "_ARRAY_DIMENSIONS": [
+                        "time",
+                        "y",
+                        "x"
+                    ]
+                },
+                fp, indent=2)
 
     async def fetch(url, session):
         async with session.get(url) as response:
@@ -81,12 +126,13 @@ def test_async(port: int, tile_size: int, dataset_path: str, var_name: str):
         # Getter function with semaphore.
         async with sem:
             data = await fetch(url, session)
-            write_data(data, chunk_name)
+            if write:
+                write_data(data, chunk_name)
+            return data
 
     def write_data(data, chunk_name):
-        # TODO: configure .zarray, so we can do:
-        # blosc = numcodecs.Blosc()
-        # data = blosc.encode(data)
+        if compress:
+            data = DEFAULT_COMPRESSOR.encode(data)
         chunk_path = os.path.join(var_path, chunk_name)
         with open(chunk_path, 'wb') as fp:
             fp.write(data)
@@ -114,11 +160,10 @@ def test_async(port: int, tile_size: int, dataset_path: str, var_name: str):
 
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(run())
-    print(f'Fetching & writing...')
     loop.run_until_complete(future)
 
 
-def test_store(port: int, tile_size: int, dataset_path: str, var_name: str):
+def _test_store(port: int, tile_size: int, dataset_path: str, var_name: str, write=False, compress=False):
     zero_tile = np.zeros(tile_size * tile_size, dtype=np.float64).tobytes()
 
     def get_chunk(cube_store: CubeStore, name: str, index: Tuple[int, ...]) -> bytes:
@@ -134,8 +179,11 @@ def test_store(port: int, tile_size: int, dataset_path: str, var_name: str):
                       chunks=(1, tile_size, tile_size))
     store.add_lazy_array(var_name, '<f8', get_chunk=get_chunk)
     ds = xr.open_zarr(store)
-    print(f'Writing {dataset_path}...')
-    ds.to_zarr(dataset_path)
+    if write:
+        ds.to_zarr(dataset_path,
+                   encoding={var_name: dict(compressor=DEFAULT_COMPRESSOR if compress else None)})
+    else:
+        ds[var_name].compute()
 
 
 def _make_tile_url(port, z, y, x):
@@ -143,34 +191,31 @@ def _make_tile_url(port, z, y, x):
 
 
 @click.command(name="tile_client")
-@click.argument('mode', type=click.Choice(['async', 'store']))
+@click.argument('name', type=click.Choice(tuple(get_tests().keys())))
 @click.option('--tile-size', help=f'Tile size. Defaults to {DEFAULT_TILE_SIZE}.',
               type=int, default=DEFAULT_TILE_SIZE)
-def tile_client(mode: str,
+def tile_client(name: str,
                 tile_size: int = None):
     """
     Run the test tile client.
     """
     port = 9092
+    var_name = 'TEST'
     with subprocess.Popen([sys.executable,
                            os.path.join(os.path.dirname(__file__), 'tile_server.py'),
+                           '--name',
+                           name,
                            '--tile-size',
                            str(tile_size),
                            '--port',
                            str(port)]) as process:
-
-        dataset_path = f'tile_client_{mode}_{tile_size}.zarr'
+        dataset_path = f'tile_client_{name}_{tile_size}.zarr'
         if os.path.exists(dataset_path):
             print(f'Removing {dataset_path}...')
             rimraf(dataset_path)
 
-        var_name = 'TEST'
-
         t1 = time.perf_counter()
-        if mode == 'async':
-            test_async(port, tile_size, dataset_path, var_name)
-        else:
-            test_store(port, tile_size, dataset_path, var_name)
+        get_tests()[name](port, tile_size, dataset_path, var_name)
         t2 = time.perf_counter()
 
         process.kill()
@@ -180,16 +225,19 @@ def tile_client(mode: str,
         num_bytes_total = NUM_TIMES * (NUM_Y_TILES * tile_size) * (NUM_X_TILES * tile_size) * num_bytes_per_value
         bytes_per_sec = num_bytes_total / time_total
 
+        print(80 * '=')
+        print(f'Test {name!r} results:')
         print()
-        print(f'num_bytes_per_value = {num_bytes_per_value}')
-        print(f'num_times = {NUM_TIMES}')
-        print(f'num_y_tiles = {NUM_Y_TILES}')
-        print(f'num_x_tiles = {NUM_X_TILES}')
-        print(f'tile_size = {tile_size}')
-        print()
-        print(f'Done after {time_total} sec.')
-        print(f'Bytes read: {num_bytes_total} ({num_bytes_total // (1024 * 1024)} MiB)')
-        print(f'Rate: {bytes_per_sec} ({bytes_per_sec // (1024 * 1024)} MiB/sec)')
+        print(f'Time: {time_total} sec.')
+        print(f'Read: {num_bytes_total // (1024 * 1024)} MiB')
+        print(f'Rate: {bytes_per_sec // (1024 * 1024)} MiB/sec')
+        print('with')
+        print(f'  num_bytes_per_value = {num_bytes_per_value}')
+        print(f'  num_times = {NUM_TIMES}')
+        print(f'  num_y_tiles = {NUM_Y_TILES}')
+        print(f'  num_x_tiles = {NUM_X_TILES}')
+        print(f'  tile_size = {tile_size}')
+        print(80 * '=')
 
 
 if __name__ == '__main__':
--- test/core/tile_server.py
+++ test/core/tile_server.py
@@ -20,8 +20,8 @@
 # SOFTWARE.
 
 import logging
-import time
 import threading
+import time
 
 import click
 import flask
@@ -29,15 +29,18 @@ import numpy as np
 
 DEFAULT_TILE_SIZE = 1024
 DEFAULT_PORT = 3030
+DEFAULT_NAME = 'test'
 
 
-def run_tile_server(tile_size: int = DEFAULT_TILE_SIZE,
+def run_tile_server(name: str = DEFAULT_NAME,
+                    tile_size: int = DEFAULT_TILE_SIZE,
                     host: str = None,
                     port: int = DEFAULT_PORT,
                     debug: bool = False):
     """
     Start the test tile server.
 
+    :param name: Test name.
     :param tile_size: Tile size to be used.
     :param host: The hostname to listen on. Set this to ``'0.0.0.0'`` to
         have the server available externally as well. Defaults to ``'127.0.0.1'``.
@@ -47,32 +50,52 @@ def run_tile_server(tile_size: int = DEFAULT_TILE_SIZE,
     tile_size = tile_size or DEFAULT_TILE_SIZE
     port = port or DEFAULT_PORT
 
+    records_file = f'tile_server_{name}_{tile_size}.csv'
+    records_fp = open(records_file, 'w')
+    records_fp.write('idx,tile,t1,t2,dt')
+    records_fp.flush()
+    record_index = 0
+
     app = flask.Flask(__name__)
 
     app.logger.setLevel(logging.INFO)
 
     start_time = time.perf_counter()
 
+    @app.route('/tile/<int:z>/<int:y>/<int:x>.data', methods=['GET'])
+    def _tile(z: int, y: int, x: int):
+        nonlocal record_index, start_time
+
+        t1 = get_millis_since_reset()
+        data = compute_tile(z, y, x)
+        t2 = get_millis_since_reset()
+
+        record_index += 1
+        record = record_index, f'{z}/{y}/{x}', t1, t2, t2 - t1, threading.current_thread().getName()
+        record_line = ','.join(map(str, record))
+        app.logger.info(record_line)
+        records_fp.write('\n' + record_line)
+        records_fp.flush()
+
+        return data
+
     def get_millis_since_reset() -> int:
         nonlocal start_time
         return round(1000 * (time.perf_counter() - start_time))
 
-    @app.route('/tile/<int:z>/<int:y>/<int:x>.data', methods=['GET'])
-    def _tile(z: int, y: int, x: int):
-        nonlocal start_time
-        t1 = get_millis_since_reset()
+    def compute_tile(z, y, x) -> bytes:
         r = np.random.RandomState(seed=(10 * (z + 10 * y) + x))
-        for i in range(10):
+        data = r.standard_normal(tile_size * tile_size)
+        for i in range(9):
             data = r.standard_normal(tile_size * tile_size)
-        data = data.tobytes()
-        t2 = get_millis_since_reset()
-        app.logger.info(f'tile {z}/{y}/{x}, {t1}, {t2}, {t2 - t1}, {threading.current_thread()}')
-        return data
+        return data.tobytes()
 
     app.run(host=host, port=port, debug=debug, threaded=True)
 
 
 @click.command(name="tile_server")
+@click.option('--name', help=f'Test name. Defaults to {DEFAULT_NAME!r}.',
+              default=DEFAULT_NAME)
 @click.option('--tile-size', help=f'Tile size. Defaults to {DEFAULT_TILE_SIZE}.',
               type=int, default=DEFAULT_TILE_SIZE)
 @click.option('--host', '-a',
@@ -82,14 +105,15 @@ def run_tile_server(tile_size: int = DEFAULT_TILE_SIZE,
 @click.option('--port', '-p', type=int, default=DEFAULT_PORT,
               help=f"The port number to listen on. Defaults to {DEFAULT_PORT}.")
 @click.option('--debug', is_flag=True, help='Output extra debugging information.')
-def tile_server(tile_size: int = None,
+def tile_server(name: str = None,
+                tile_size: int = None,
                 host: str = None,
                 port: int = None,
                 debug: bool = False):
     """
     Start the test tile server.
     """
-    run_tile_server(tile_size=tile_size, host=host, port=port, debug=debug)
+    run_tile_server(name=name, tile_size=tile_size, host=host, port=port, debug=debug)
 
 
 if __name__ == '__main__':
